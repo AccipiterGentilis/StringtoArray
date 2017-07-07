5eb3309456af842fa278ab2ca27084770f7ce6b5
AccipiterGentilis <AccipiterGentilisMY@gmail.com>
2017-07-07 20:48:24 +0900

    Update unicodesplit.js
    put unicodesplit in toArray

--- unicodesplit.js
+++ unicodesplit.js
@@ -1,39 +1,39 @@
-String.prototype.unicodesplit = function() {
-    var _stringarr = new Array(),
-        _going = true,
-        _strindex = 0,
-        _arrmodify = {
-            add: function(str) {
-                _stringarr[_stringarr[_strindex] ? ++_strindex : _strindex] = str;
-            },
-            edit: function(str) {
-                _stringarr[_strindex] = _stringarr[_strindex] ? _stringarr[_strindex] + _unicode : str;
-            }
-        }
-
-    for (var i = 0; i < this.length; i++) {
-        var _code = this.charCodeAt(i),
-            _point = this.codePointAt(i),
-            _unicode = String.fromCharCode(_code),
-            _special = [0x200d, 0xfe0e, 0xfe0f, 0x1f3fb, 0x1f3fc, 0x1f3fd, 0x1f3fe, 0x1f3ff];
-
-        if (_special.indexOf(_point) > -1) {
-            _arrmodify.edit(_unicode);
-        } else {
-            if (_going) {
-                _arrmodify.add(_unicode);
-            } else {
-                _arrmodify.edit(_unicode);
-            }
-        }
-        _going = !(_point === 0x200d || _point > _code);
-    }
-    return _stringarr;
-};
-
 String.prototype.toArray = function(system, prefix) {
-    if (!system) {
-        return this.unicodesplit();
+    var unicodesplit = function() {
+        var _stringarr = new Array(),
+            _going = true,
+            _strindex = 0,
+            _arrmodify = {
+                add: function(str) {
+                    _stringarr[_stringarr[_strindex] ? ++_strindex : _strindex] = str;
+                },
+                edit: function(str) {
+                    _stringarr[_strindex] = _stringarr[_strindex] ? _stringarr[_strindex] + _unicode : str;
+                }
+            }
+
+        for (var i = 0; i < this.length; i++) {
+            var _code = this.charCodeAt(i),
+                _point = this.codePointAt(i),
+                _unicode = String.fromCharCode(_code),
+                _special = [0x200d, 0x200c, 0xfe0e, 0xfe0f, 0x1f3fb, 0x1f3fc, 0x1f3fd, 0x1f3fe, 0x1f3ff];
+
+            if (_special.indexOf(_point) > -1) {
+                _arrmodify.edit(_unicode);
+            } else {
+                if (_going) {
+                    _arrmodify.add(_unicode);
+                } else {
+                    _arrmodify.edit(_unicode);
+                }
+            }
+            _going = !(_point === 0x200d || _point > _code) || _point === 0x200c;
+        }
+        return _stringarr;
+    };
+
+    if (!system || !Number(system) || !(Number(system) >= 2 && Number(system) <= 36)) {
+        return unicodesplit.call(this);
     } else {
         var _decarr = new Array(),
             _codearr = new Array(),
@@ -68,8 +68,6 @@ String.prototype.toArray = function(system, prefix) {
                     _retarr = _decarr.map(function(val) {
                         return val.toString(Number(system));
                     });
-                } else {
-                    _retarr = this.unicodesplit();
                 }
                 break;
         }
